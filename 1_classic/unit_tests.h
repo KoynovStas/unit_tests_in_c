14f7e3f5427780705965666c8628070b8f54c3b2
KoynovStas <skojnov@yandex.ru>
2017-06-05 12:02:35 +0300

    Refactoring variant 1

--- 1_classic/unit_tests.h
+++ 1_classic/unit_tests.h
@@ -2,6 +2,10 @@
 #define UNIT_TESTS_HEADER
 
 
+#include <stdio.h>
+#include <stdlib.h> //for exit
+
+
 
 
 
@@ -38,7 +42,24 @@ typedef  int (*ptest_func)(struct test_info_t  *test_info);
 
 
 
-void run_tests(ptest_func *tests, size_t count_tests);
+static void run_tests(ptest_func *tests, size_t count_tests)
+{
+    struct test_info_t test_info;
+    unsigned int i;
+
+    for(i=0; i < count_tests; ++i)
+    {
+
+        if( tests[i](&test_info) == TEST_BROKEN)
+        {
+            printf("test:  %s  in file: %s  is broken\n", test_info.func_name, test_info.file_name);
+            exit(1);
+        }
+    }
+
+
+    printf("all test passed  count tests = %ld\n", count_tests);
+}
 
 
 
